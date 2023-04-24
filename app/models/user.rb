a096363a565176a9d1a9a45106a3e67f09c7ba79
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-04-24 20:53:59 +0200

    Add entity and group model also add their associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: 250 }
+
+  has_many :groups, dependent: :destroy
+  has_many :entities, dependent: :destroy
 end
