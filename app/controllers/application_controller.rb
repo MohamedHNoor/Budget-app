fdb36c82593929d092bde2a681165c817128ad8c
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-04-24 14:36:57 +0200

    Add user model and authentication using devise gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:name, :email, :password, :password_confirmation)
+    end
+
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit(:name, :email, :password, :password_confirmation, :current_password)
+    end
+  end
 end
