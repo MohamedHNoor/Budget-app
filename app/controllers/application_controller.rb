264dc07a1b41fb039550c9179d1a5f3c3558da2f
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-04-24 20:52:26 +0200

    Add entities and groups controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
 
-  def configure_permitted_parameters
+  def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
       user_params.permit(:name, :email, :password, :password_confirmation)
     end
